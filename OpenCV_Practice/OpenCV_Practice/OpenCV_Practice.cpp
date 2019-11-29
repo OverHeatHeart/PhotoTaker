28075a192f0a560f96250b514b0307b6ea5ef928
heatheart <ralenore91@gmail.com>
2019-11-29 11:44:44 +0900

    주석 추가

--- OpenCV_Practice/OpenCV_Practice/OpenCV_Practice.cpp
+++ OpenCV_Practice/OpenCV_Practice/OpenCV_Practice.cpp
@@ -9,22 +9,26 @@ using namespace cv;
 int main()
 {
 	bool switcher = false;
+
 	VideoCapture cap;
 	cap.open(0);
 	if (!cap.isOpened())
 	{
-		cerr << "카메라가 없어염" << endl;
+		cerr << "카메라 없음" << endl;
 		return -1;
 	}
+
 	Mat originalFrame;
 	Mat cutFrame;
-	int count = 0;
-	int num = 490;
+
+	int count = 0;	//초를 셀 변수
+	int num = 0;	//0번부터 파일 이름
 	while (true)
 	{
 		cap >> originalFrame;
 		if (originalFrame.empty()) break;
 		cutFrame = originalFrame(Rect(70, 70, 300, 300));
+		//해당 프레임에서 특정 영역을 뽑아냄
 		rectangle(originalFrame, Rect(70, 70, 300, 300), Scalar(255, 255, 255), 2);
 		imshow("skin", cutFrame);
 		imshow("origin", originalFrame);
@@ -37,17 +41,19 @@ int main()
 				cout << "HELLO" << num << endl;
 				string filename = format("D:\\Datasets\\Handlays\\Hands%d.jpg", num);
 				num++;
+				//잘라낸 프레임을 저장함
 				imwrite(filename, cutFrame);
 			}
 		}
 
 		int keycode = waitKey(5);
 
-		if (keycode == 'a') // a키를 누르면
+		if (keycode == 'a')			// a키를 누르면
 		{
 			switcher = !switcher;
 		}
-		else if (keycode == 27) {
+		else if (keycode == 27)		//esc를 누르면
+		{					
 			break;
 		}
 	}
